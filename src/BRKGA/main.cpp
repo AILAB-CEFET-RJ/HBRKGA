fe8e4c6da502bd9b082355eed8cb76f8c3a3903a
Mateus Devino <mateus@mdevino.com>
2021-04-06 21:47:52 -0300

    .gitignore and parametrized main

--- src/BRKGA/main.cpp
+++ src/BRKGA/main.cpp
@@ -4,15 +4,30 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
 
-	cout<<"Começou!"<<endl;
+	cout << "Começou!" << endl;
 
-	AG Meta(100,1000,0.2,0.1,0.7,1,1,0,0,100,2,1000);
+	AG Meta(100, 1000, 0.2, 0.1, 0.7, 1, 1, 0, 0, 100, 2, 1000);
 
 	Meta.solve();
 
-	cout<<"Terminou!"<<endl;
+	cout << "Terminou!" << endl;
 
-	return 0;	
+	return 0;
+}
+
+int my_main(unsigned n, unsigned p, double pe, double pm, double rhoe,
+				 unsigned k, unsigned maxT, long unsigned rngSeed, unsigned generation,
+				 unsigned xIntvl, unsigned xNumber, unsigned maxGens)
+{
+
+	cout << "Começou!" << endl;
+	AG Meta(n, p, pe, pm, rhoe, k, maxT, rngSeed, generation, xIntvl, xNumber,
+					maxGens);
+
+	Meta.solve();
+
+	cout << "Terminou!" << endl;
 }
